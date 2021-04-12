dbb2d70ac34715169261739cfab4c05eb3ea5ff5
Anna Maria <aannamariabr@outlook.com>
2021-04-12 10:56:03 -0300

    Mais funções do sequelize

--- instagram.js
+++ instagram.js
@@ -1,17 +1,48 @@
-const { Usuario, Post, Comentarios, sequelize } = require('./models');
+const { Usuario, Post, Comentarios, sequelize, Sequelize } = require('./models');
 
-Usuario.findAll()
+// Usuario.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
+
+// Post.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
+
+// Comentarios.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
+
+//Buscar todos os usuarios que tem a letra A no nome
+const op = Sequelize.Op;
+Usuario.findAll({
+    where:{
+        nome:{[op.like ]: 'a%'}
+    }
+})
 .then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
+    console.log(resultado.map(user => user.toJSON()));
 });
 
-Post.findAll()
+//Buscar todos os usuarios que não tem a letra A no nome
+const op2 = Sequelize.Op;
+Usuario.findAll({
+    where:{
+        nome:{[op2.notLike]: 'a%'}
+    }
+})
 .then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
+    console.log(resultado.map(user => user.toJSON()));
 });
 
-Comentarios.findAll()
+//Buscar todos os comentários e exibir 2 por vez
+Comentarios.findAll({
+    limit: 2
+})
 .then((resultado) => {
-    console.table(resultado.map(user => user.toJSON()));
+    console.log(resultado.map(user => user.toJSON()));
 });
+
 
